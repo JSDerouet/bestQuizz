6a041c0bbde7525e3281afe4febe5a5cbc260987
Alexandre DERET <[allex.deret@gmail.com](mailto:allex.deret@gmail.com)>
2023-03-24 09:24:44 +0100

    fix html & JavaScript

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Récupération des éléments "interactifs"
 const resultPage = document.querySelector(".result");
-const resultWindow = document.querySelector(".resultwindow");
-const closeResult = document.querySelector(".closemodale");
-const submitForm = document.querySelector("#submit");
+const resultWindow = document.querySelector(".result_window");
+const closeResult = document.querySelector(".close_modale");
+const submitForm = document.querySelector(".submit");
 const form = document.querySelector("form");
-const titreModale = document.querySelector(".resultwindowtitle");
-const textModale = document.querySelector(".resultwindowtext");
+const titreModale = document.querySelector(".result_window_title");
+const textModale = document.querySelector(".result_window_text");
 
 submitForm.addEventListener('click', function (event) {
     event.preventDefault();
     const nbPoints = verifyAnswers();
     displayResultWindow(nbPoints);
-})
+});
 
 // Le bouton doit cacher la modale "Résultat"
 closeResult.addEventListener("click",  function() {
@@ -41,20 +41,43 @@ function displayResultWindow (points) {
 // Fonction qui cache la modale
 function hideResultWindow () {
     resultPage.style.display = "none";
-    resultWindow.className = "resultwindow";
-}
+    resultWindow.className = "result_window";
+};
 
 // Fonction qui vérifie le nombre de points obtenus :
 function verifyAnswers () {
     let points = 0;
     if ( form["option"].value==="Khorne"){
         points++;
+        console.log("Bing 1");
     }
-    if ((form["car"][0].checked) && (form["car"][2].checked) && (form["car"][3].checked) && !(form["car"][1].checked) && !(form["car"][4].checked) && !(form["car"][5].checked)){
+    // form["car"][0].value = "Renault", etc
+    if ((form["car"][0].checked) && (form["car"][2].checked) && (form["car"][3].checked) && (!(form["car"][1].checked)) && (!(form["car"][4].checked)) && (!(form["car"][5].checked))){
         points++;
     }
-    if ( form["timbre"].value==="Philatélie"){
+    if ( form["timbre"].value==="La philatélie"){
         points++;
     }
     return points;
-}
+};
+
+// Constructeur de Questions
+/* On a besoin de :
+- Le type : [CheckBox - Radio]
+- Un intitulé
+- Les réponses : Unique ou Array
+*/
+/*
+function Question(type, question, answer) {
+    this.type = type;
+    this.question = question;
+    this.answer = answer;
+    
+    this.verify = function (array){
+        if (array.equals)
+    }
+};
+
+function verifyQuestion() {
+
+};*/
