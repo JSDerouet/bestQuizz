987bc6df1334a7c4d7c5b7fb5118aa90e2781e11
Alexandre DERET <[allex.deret@gmail.com](mailto:allex.deret@gmail.com)>
2023-03-24 10:07:53 +0100

    travail css et phrases réponses

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const submitForm = document.querySelector(".submit");
 const form = document.querySelector("form");
 const titreModale = document.querySelector(".result_window_title");
 const textModale = document.querySelector(".result_window_text");
+const textAnswers = document.querySelectorAll(".verif");
 
 submitForm.addEventListener('click', function (event) {
     event.preventDefault();
+    cleanVerifs();
     const nbPoints = verifyAnswers();
     displayResultWindow(nbPoints);
 });
@@ -49,17 +51,41 @@ function verifyAnswers () {
     let points = 0;
     if ( form["option"].value==="Khorne"){
         points++;
-        console.log("Bing 1");
+        textAnswers[0].textContent = "Réponse correcte !";
+        textAnswers[0].classList.toggle("correct_answer");
+    }
+    else {
+        textAnswers[0].textContent = "Réponse incorrecte !";
+        textAnswers[0].classList.toggle("incorrect_answer");
     }
     // form["car"][0].value = "Renault", etc
     if ((form["car"][0].checked) && (form["car"][2].checked) && (form["car"][3].checked) && (!(form["car"][1].checked)) && (!(form["car"][4].checked)) && (!(form["car"][5].checked))){
         points++;
+        textAnswers[1].textContent = "Réponse correcte !";
+        textAnswers[1].classList.toggle("correct_answer");
+    }
+    else {
+        textAnswers[1].textContent = "Réponse incorrecte !";
+        textAnswers[1].classList.toggle("incorrect_answer");
     }
     if ( form["timbre"].value==="La philatélie"){
         points++;
+        textAnswers[2].textContent = "Réponse correcte !";
+        textAnswers[2].classList.toggle("correct_answer");
+    }
+    else {
+        textAnswers[2].textContent = "Réponse incorrecte !";
+        textAnswers[2].classList.toggle("incorrect_answer");
     }
     return points;
 };
+
+// Fonction qui "nettoie" les textes de la classe verif
+function cleanVerifs () {
+    for ( let i = 0; i < textAnswers.length; i++){
+        textAnswers[i].className = "verif";
+    }
+}
 
 // Constructeur de Questions
 /* On a besoin de :
